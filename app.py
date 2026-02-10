3c51725c9f4c85b0ea8d65363cd452902ec2943a
hrzin13 <pabloribeiro40001824@gmail.com>
2026-02-10 16:53:38 -0300

    app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def upload_drive(service, file_obj, name, folder_id, mime):
 st.title("📄 Scanner Turbo IRPF")
 
 # ⚠️⚠️⚠️ COLOQUE O CÓDIGO DA SUA PASTA AQUI EMBAIXO ⚠️⚠️⚠️
-FOLDER_ID_RAIZ = "COLOQUE_SEU_ID_AQUI" 
+FOLDER_ID_RAIZ = "1hxtNpuLtMiwfahaBRQcKrH6w_2cN_YFQ" 
 
 if configurar_apis():
     if "cpf_atual" not in st.session_state: st.session_state["cpf_atual"] = ""
